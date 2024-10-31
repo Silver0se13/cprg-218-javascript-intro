8be54e4f4210580e758a3fd19ca204dac213c53e
Silver0se13 <gurleens0013@gmail.com>
2024-10-31 17:39:19 -0600

    Added light toggle
    Added light toggle

--- intro.js
+++ intro.js
@@ -7,9 +7,22 @@ secret.addEventListener('click', function(){
 light_on.addEventListener('click', function(){
     console.log("let there be light!");
     document.getElementById("lightbulb").src="lightbulb_on.webp";
+
+    //When this button is pressed, turn the 'light_on' button to be invisible, and the 'light_off' button to be visible
+    document.getElementById('light_on').style.display="none";
+    document.getElementById('light_off').style.display="initial";
+    
+    document.body.classList.toggle('light');
 })
 
 light_off.addEventListener('click', function(){
     console.log("let there be darkness");
     document.getElementById("lightbulb").src="lightbulb_off.webp";
+
+    //When this button is pressed, turn the 'light_off' button to be invisible, and the 'light_on' button to be visible
+    document.getElementById('light_off').style.display="none";
+    document.getElementById('light_on').style.display="initial";
+
+    document.body.classList.toggle('light');
 })
+
