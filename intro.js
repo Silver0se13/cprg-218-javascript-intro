a87d146dbfc5fd6548ee475c87907e6343b1a70e
Silver0se13 <gurleens0013@gmail.com>
2024-10-31 16:38:21 -0600

    Added light on and light off buttons,
    Coded light on and light off in JS

--- intro.js
+++ intro.js
@@ -1,3 +1,15 @@
 secret.addEventListener('click', function(){
     alert("This is the secret message... not so secret is it?");
 })
+
+//When the user clicks the 'light_on' button, change the 'lightbulb' image to show the lightbulb on version
+
+light_on.addEventListener('click', function(){
+    console.log("let there be light!");
+    document.getElementById("lightbulb").src="lightbulb_on.webp";
+})
+
+light_off.addEventListener('click', function(){
+    console.log("let there be darkness");
+    document.getElementById("lightbulb").src="lightbulb_off.webp";
+})
